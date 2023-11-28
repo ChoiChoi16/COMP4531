70ddc1c2ccbd0527bce3a1bdba784490487249e1
qbgXyn <qbgxyn@gmail.com>
2023-11-28 22:15:38 +0800

    tidy up code

--- code/Gen_Phase_&_Distance.py
+++ code/Gen_Phase_&_Distance.py
@@ -7,49 +7,51 @@ import time
 fell = False
 filename = input("what is the filename of the wav audio?")
 filename = filename + '.wav'
-# read audio file recorded by Raspberry pi
-file = wave.open(filename, 'rb')
-# get sampling frequency
-sf = file.getframerate()
-# get audio data total length
-n_length = file.getnframes()
-# read audio data
-audio_data_raw = file.readframes(n_length)
-# transfer to python list
-audio_data_raw = list(audio_data_raw)
-# transfer to numpy array
-audio_data_raw = np.array(audio_data_raw).astype(np.int8)
-# set the data type to int16
-audio_data_raw.dtype = 'int16'
+
+# Read audio file recorded by Raspberry Pi using a context manager
+with wave.open(filename, 'rb') as file:
+    # Get sampling frequency and audio data total length
+    sf = file.getframerate()
+    n_length = file.getnframes()
+
+    # Read audio data and convert to int16
+    audio_data_raw = np.frombuffer(file.readframes(n_length), dtype=np.int16)
+
+
 # calculate audio length in second
 audio_data_raw_total_time = n_length/sf
-# close the file
-file.close()
+
 
 # cut the middle part of the audio data
 time_offset = 2
+time_offset_point = int(time_offset * sf)
 total_time = np.int32(np.ceil(audio_data_raw_total_time - time_offset - 2))
 total_point = total_time * sf
-time_offset_point = time_offset * sf
 audio_data = audio_data_raw[range(time_offset_point,time_offset_point+total_point)]
 
-# set frequency
+# set frequency and calculate time t
 freq = 18000
-# calculate time t
 t = np.arange(total_point)/sf
+
 # get the cos and sin used in demodulation
 signal_cos = np.cos(2*np.pi*freq*t)
 signal_sin = np.sin(2*np.pi*freq*t)
+
 # get a low-pass filter
 b, a = signal.butter(3, 50/(sf/2), 'lowpass')
+
 # multiply received signal and demodulate signal
 signalI = signal.filtfilt(b, a, audio_data*signal_cos)
 signalQ = signal.filtfilt(b, a, audio_data*signal_sin)
+
 # remove the static vector
-signalI = signalI - np.mean(signalI)
-signalQ = signalQ - np.mean(signalQ)
+signalI -= np.mean(signalI)
+signalQ -= np.mean(signalQ)
+
 # calculate the phase angle
 phase = np.arctan(signalQ/signalI)
+
+
 # plot the IQ figure
 '''plt.plot(signalI, signalQ)
 plt.xlabel('I')
@@ -59,36 +61,36 @@ plt.show()'''
 '''plt.plot(t, phase)
 plt.xlabel('t/s')
 plt.show()'''
+
+
 # unwrap the phase
 phase = np.unwrap(phase*2)/2
+
+
 # plot the unwraped phase
 '''plt.plot(t, phase)
 plt.show()'''
 
-# fall detection
-fell_duration = 0
-for i in phase:
-    if (i >= 100):
-        fell_duration += 1
-    else:
-        fell_duration = 0
-        continue
-    if (fell_duration >= 120000):
-        fell = True
-        break
 
-# calculate the wave length
+# fall detection
+fall_duration = np.sum(phase >= 100)
+fell = fall_duration >= 120000
+
+# calculate the wave length and distance
 wave_length = 342/freq
-# calculate the distance
 distance = phase/2/np.pi*wave_length/2
+
+
 # plot the distance
 '''plt.plot(t, distance)
 plt.xlabel('time/s')
 plt.ylabel('distance/m')
 plt.show()'''
+
+
 # alert
 print("")
-if (fell == True):
+if fell:
     print("user has fell down! Calling 999...")
 else:
     print("user is fine.")
